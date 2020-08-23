4572407a7d32a4a6b8fb82a696e04d0d62923928
HarshithaPeddakurma <harshitha.89@gmail.com>
2020-08-23 17:30:40 +1000

    Assignment

---
+++ LifeSpanAssignment/src/LifeSpanAssignment.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class LifeSpanAssignment {
+
+	public static void main(String[] args) throws IOException {
+		LifeSpanAssignment lifespan = new LifeSpanAssignment();
+	    Cont.doWork();
+	}
+}
